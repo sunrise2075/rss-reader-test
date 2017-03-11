fd98f941102b6c22fb700a434e7b221cfb7ce792
sunrise2075 <sunrise2075@163.com>
2017-03-11 22:49:17 +0800

    add two test use cases

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,20 +45,25 @@ $(function() {
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
+    describe("The menu", function(){
 
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
 
-         /* TODO:
-          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-          * 再次点击的时候是否隐藏。
-          */
+        /* TODO:
+         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+         * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+         * 再次点击的时候是否隐藏。
+         */
+    });
+
+
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
+    describe("Initial Entries", function(){
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -67,10 +72,12 @@ $(function() {
          * 和异步的 done() 函数。
          */
 
-    /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
+        /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
 
         /* TODO:
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
+    })
+
 }());
